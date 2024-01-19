ede5c9e510bbc15016a1813216347d2c9db63748
gutierrez-anthony <gutierrez.anthony@student.greenriver.edu>
2024-01-19 13:54:42 -0800

    Route established in Controller.

--- src/main/java/edu/greenriver/sdev/numbergenerator/controllers/NumberController.java
+++ src/main/java/edu/greenriver/sdev/numbergenerator/controllers/NumberController.java
@@ -1,13 +1,21 @@
 package edu.greenriver.sdev.numbergenerator.controllers;
 
+import edu.greenriver.sdev.numbergenerator.services.NumberService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class NumberController {
+    private NumberService service;
+
+    public NumberController(NumberService service) {
+        this.service = service;
+    }
 
     @RequestMapping("randnum")
-    public String genNumber(){
-        return "randnum";
+    public String genNumber(Model modelVars){
+        modelVars.addAttribute("num", service.between(1, 20));
+        return "num";
     }
 }
