4a24ca66198b8944cbab16d820df9ca2fe79600f
gutierrez-anthony <gutierrez.anthony@student.greenriver.edu>
2024-01-19 15:03:40 -0800

    Multiple number generator added with variable number of numbers to generate.

--- src/main/java/edu/greenriver/sdev/numbergenerator/services/NumberService.java
+++ src/main/java/edu/greenriver/sdev/numbergenerator/services/NumberService.java
@@ -2,11 +2,21 @@ package edu.greenriver.sdev.numbergenerator.services;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @Service
 public class NumberService {
     public int between(int low, int high) {
         return new Random().nextInt(low, high);
+    }
+
+    public List<Integer> betweenLarge(int size, int low, int high) {
+        List<Integer> numList = new ArrayList<>();
+        for(int i = 0; i < size; i++) {
+            numList.add(new Random().nextInt(low, high));
+        }
+        return numList;
     }
 }
